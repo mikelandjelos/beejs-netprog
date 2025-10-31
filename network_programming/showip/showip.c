2fc4aabd0ac6e2691fd224173866f6b852032f95
mikelandjelos <mihajlo.madic@gmail.com>
2025-10-31 23:59:51 +0100

    finished showip

--- network_programming/showip/showip.c
+++ network_programming/showip/showip.c
@@ -2,11 +2,23 @@
  ** showip.c
  **
  ** Show IP addresses for a host given on the command line
+ **
+ ** Example usage: `./dist/showip "google.com" ipver4 | xargs ping -c 4`
+ ** Output:
+ ** ```txt
+ ** PING 142.250.180.206 (142.250.180.206) 56(84) bytes of data.
+ ** 64 bytes from 142.250.180.206: icmp_seq=1 ttl=115 time=12.6 ms
+ ** 64 bytes from 142.250.180.206: icmp_seq=2 ttl=115 time=12.3 ms
+ ** 64 bytes from 142.250.180.206: icmp_seq=3 ttl=115 time=12.4 ms
+ ** 64 bytes from 142.250.180.206: icmp_seq=4 ttl=115 time=12.4 ms
+ **
+ ** --- 142.250.180.206 ping statistics ---
+ ** 4 packets transmitted, 4 received, 0% packet loss, time 3004ms
+ ** rtt min/avg/max/mdev = 12.326/12.424/12.575/0.092 ms
+ ** ```
  */
 
-#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
-#endif
 
 #include <stdio.h>
 #include <string.h>
@@ -19,30 +31,51 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+#define ERROR_BAD_ARGUMENTS -1
+
+typedef enum IpVersion {
+    Version4 = AF_INET,
+    Version6 = AF_INET6,
+    Unspecified = AF_UNSPEC,
+} IpVersion;
+
 int main(int argc, char *argv[])
 {
     struct addrinfo hints, *res, *p;
     int status;
     char ipstr[INET6_ADDRSTRLEN];
 
-    if (argc != 2) {
-        fprintf(stderr, "usage: showip <hostname>\n");
-        exit(-1);
+    if (argc < 2 || argc > 3)
+        fprintf(stderr, "usage: showip <hostname> [IPv4 | IPv6]\n"),
+            fflush(stderr), exit(ERROR_BAD_ARGUMENTS);
+
+    IpVersion ipver = Unspecified;
+    if (argc == 3) {
+        int len = 0;
+        char *s = argv[2];
+        while (*s)
+            ++len, ++s;
+        --len, --s;
+        if (len > 5)
+            fprintf(stderr, "version parameter needs to be max length 5\n"),
+                fflush(stderr), exit(ERROR_BAD_ARGUMENTS);
+        if (*s == '4' || *s == '6')
+            ipver = *s == '4' ? Version4 : Version6;
+        else
+            fprintf(stderr, "ip version must be 4 or 6, but provided %c\n", *s),
+                fflush(stderr), exit(ERROR_BAD_ARGUMENTS);
     }
 
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC; // IP version agnostic
+    hints.ai_family = ipver; // IP version agnostic
     hints.ai_socktype = SOCK_STREAM;
 
-    if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0) {
+    if ((status = getaddrinfo(argv[1], "443", &hints, &res)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
         exit(-2);
     }
 
-    printf("IP addresses for %s:\n\n", argv[1]);
-
     void *addr;
-    char *ipver;
     struct sockaddr_in *ipv4;
     struct sockaddr_in6 *ipv6;
     for (p = res; p != NULL; p = p->ai_next) {
@@ -51,15 +84,13 @@ int main(int argc, char *argv[])
         if (p->ai_family == AF_INET) {
             ipv4 = (struct sockaddr_in *)p->ai_addr;
             addr = &(ipv4->sin_addr);
-            ipver = "IPv4";
         } else {
             ipv6 = (struct sockaddr_in6 *)p->ai_addr;
             addr = &(ipv6->sin6_addr);
-            ipver = "IPv6";
         }
 
         inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
-        printf("    %s: %s\n", ipver, ipstr);
+        printf("%s\n", ipstr);
     }
 
     freeaddrinfo(res);
